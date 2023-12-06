2e05502310cc656c6e43c8f2702fb04ae1e68581
Nhat <hoathinhvietnam2016@gmail.com>
2023-12-06 17:01:20 +0700

    remove file

--- knn.py
+++
@@ -1,9 +0,0 @@
-
-from sklearn import datasets
-class KNN:
-    def __init__(self,k=3):
-        self.k=k
-    def fit(self,x,y):
-        pass
-    def predict(self,x):
-        pass
--- mathplotBlib.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-x=[3,5]
-y=[7,9]
-segment=plt.plot(x, y)
-plt.show(segment)
-plt.show()
-def stored(*args, **kwargs):
-    return(args)
